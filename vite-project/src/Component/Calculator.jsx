c3c68749dff7d46a4fd17ab0374ae96d7df1f4d4
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2024-01-29 21:39:17 +0530

    css

--- vite-project/src/Component/Calculator.jsx
+++ vite-project/src/Component/Calculator.jsx
@@ -103,7 +103,7 @@ const Calculator = () => {
               <button onClick={()=>{handleClick("4")}}>4</button>
                    <button onClick={()=>{handleClick("5")}}>5</button>
                    <button onClick={()=>{handleClick("6")}}>6</button>
-                   <button onClick={()=>{handleClick("")}}></button>
+                   <button onClick={()=>{handleClick("")}}>*</button>
                </div>
                <div  className='buttonContainer'>
                     <button onClick={()=>{handleClick("7")}}>7</button>
